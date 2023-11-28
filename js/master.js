204453c938414b1afd2c941585214e5c5d577c7c
FAwzzyy <ahmedfawzyy18@gmail.com>
2023-11-28 16:37:39 +0200

    my changes

--- js/master.js
+++ js/master.js
@@ -5,16 +5,16 @@ let homepage = document.querySelector(".home-page");
 let ImgsArray =["01.gpj","02.gpj","03.gpj","04.gpj","05.gpj"];
 
 setInterval(() => {
-    //get Random Number 
-let randomNumber =Math.floor(Math.random()* ImgsArray.length);
+ //get Random Number 
+    let randomNumber =Math.floor(Math.random()* ImgsArray.length);
 
-    console.log(randomNumber);
+        console.log(randomNumber);
 
-    //changing background imgs url
-homepage.style.backgroundImgs = 'url("imgs/'+ imgsArray[randomNumber]+'")';
+        //changing background imgs url
+    homepage.style.backgroundImgs = 'url("imgs/'+ imgsArray[randomNumber]+'")';
 
 
-} ,1000)
+    } ,1000)
 
 
 
